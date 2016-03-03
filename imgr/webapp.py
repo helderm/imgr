14ab0d32c070a52752dc6edbb2c879c7add94704
Helder Martins <heldergaray@gmail.com>
2016-03-03 22:30:56 +0100

    Removing periodic callback from webserver

--- imgr/webapp.py
+++ imgr/webapp.py
@@ -10,22 +10,13 @@ class MainHandler(RequestHandler):
     def get(self):
         self.write('Hello, world!')
 
-
-def my_callback():
-    syncfs.delay('/home/helder/git/imgr/imgr/')
-
-
 def main():
     define("host", default="127.0.0.1", help="Host IP")
     define("port", default=8080, help="Port")
     tornado.options.parse_command_line()
 
     application = Application([(r"/", MainHandler),])
-
     application.listen(options.port, options.host)
-
-    periodic = tornado.ioloop.PeriodicCallback(my_callback, 10000)
-    periodic.start()
 
     tornado.ioloop.IOLoop.instance().start()
 
