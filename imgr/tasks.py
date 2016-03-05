654e590e145b429291ac7afeae019523f6e41493
Helder Martins <heldergaray@gmail.com>
2016-03-05 15:53:52 +0100

    Added support for other image types

--- imgr/tasks.py
+++ imgr/tasks.py
@@ -5,8 +5,10 @@ from celery import Celery
 from datetime import timedelta
 import os
 from pymongo import MongoClient
+from celery.utils.log import get_task_logger
 
-# instantiate Celery object
+# init celery
+logger = get_task_logger(__name__)
 mongodb_url = os.getenv('MONGODB_URL', 'mongodb://localhost:27017/')
 app = Celery(include=[ 'imgr.tasks' ], broker=mongodb_url + 'celery')
 app.config_from_object('imgr.celeryconfig')
@@ -32,24 +34,26 @@ def syncfs(path):
         ftype = mt.guess_type(fname)
         if ftype[0] is None:
             # ignore unrecognized file
+            logger.warn('Unrecognized file [{0}]!'.format(fname))
             continue
 
         fdoc = col.find_one({'name': fname})
         
         # if file is not in db, add it
         if not fdoc:
-
             fdoc = {'_id': str(uuid.uuid4()),
                     'name': fname,
                     'type': ftype[0],
                     'del': False,
                     'meta': {} }
 
+            logger.info('Inserting new file [{0}]...'.format(fname))
             col.insert(fdoc)
             continue
 
         # if file is in db, and 'remove' tag is true, remove file
         if fdoc['del'] == True:
+            logger.info('Removing file [{0}]...'.format(fname))
             os.remove(path + fname)
             col.remove(fdoc)
 
